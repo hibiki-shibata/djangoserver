3905a478973ff8bf0ab6a8e088d91e3aa605b749
Hibiki Shibata <hibiki.shibata@wolt.com>
2025-05-16 02:05:41 +0900

    serializer coded

---
+++ drfBackend/entryPoint/views/serializer.py
@@ -0,0 +1,12 @@
+# https://www.django-rest-framework.org/api-guide/serializers/
+
+from rest_framework import serializers
+from ..models import AnswerAndKeywords
+
+class clientReqSerializer(serializers.Serializer):
+    class Meta:
+        model = AnswerAndKeywords
+        # fields = '__all__' # Better to avoid not to handle unknow field from clients' req.
+        fields = ['keywords', 'answer', 'timeStamp']
+        read_only_fields = ['timeStamp']
+    # created = serializers.DateTimeField()
--- drfBackend/entryPoint/views/views.py
+++ drfBackend/entryPoint/views/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
-
 # Create your views here.
+
+# DRF
+# https://www.django-rest-framework.org/api-guide/views/#api_view
+# https://www.django-rest-framework.org/api-guide/authentication/
+
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+# https://www.django-rest-framework.org/api-guide/permissions/
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def get_from_database(request):
+    return Response({"message": "Hello, world!"})

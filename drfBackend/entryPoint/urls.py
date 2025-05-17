0ee2d0dfb99cdd63e03a6201823c1e984b05ea88
Hibiki Shibata <hibiki.shibata@wolt.com>
2025-05-18 05:28:16 +0900

    drf view classified

--- drfBackend/entryPoint/urls.py
+++ drfBackend/entryPoint/urls.py
@@ -1,14 +1,12 @@
 from django.urls import path
-from .views.views import get_keywordsAnswer_from_postgres, post_keywordsAnswer_from_postgres, delete_keywordsAnswer_from_postgres
+from .views.views import answerAndKeywordsViewSet
 
 from graphene_django.views import GraphQLView
 from django.views.decorators.csrf import csrf_exempt
 from .views.gql_schema import schema
 
 urlpatterns = [
-    path('getresponse', get_keywordsAnswer_from_postgres, name='get_keywordsAndAswer'),
-    path('postresponse', post_keywordsAnswer_from_postgres , name='post_keywordsAndAswer'),
-    path('deleteresponse',  delete_keywordsAnswer_from_postgres , name='delete_keywordsAndAswer'),
+    path('restapi', answerAndKeywordsViewSet.as_view(), name='postgres_keywordsAndAswer'),
     path("graphql", csrf_exempt(GraphQLView.as_view(graphiql=True,  schema=schema))), #schema = schema.. not necessary bc I already specifed it in settings.py
  
 ]
--- drfBackend/entryPoint/views/views.py
+++ drfBackend/entryPoint/views/views.py
@@ -10,6 +10,7 @@ from django.core.exceptions import ValidationError
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.views import APIView
 
 from .serializer import clientGetReqSerializer, serverResSerializer
 from ..models import AnswerAndKeywords
@@ -18,72 +19,141 @@ from ..models import AnswerAndKeywords
 from rest_framework.decorators import api_view, permission_classes
 # from rest_framework.permissions import IsAuthenticated
 
+    
 
-# @permission_classes([IsAuthenticated])
-@api_view(['GET'])
-def get_keywordsAnswer_from_postgres(request):
-    try:
-        print(request)
-        print("GET_keywordsAnswer_from_postgres function called")
-        # Assuming you want to retrieve all records from the AnswerAndKeywords model
-        dataFromDatavase = AnswerAndKeywords.objects.all()
-        hibikiSerializer = clientGetReqSerializer(dataFromDatavase, many=True)
-        return Response(hibikiSerializer.data, status=status.HTTP_200_OK)
+class answerAndKeywordsViewSet(APIView):    
+
+    def get(self, request):
+        try:
+            print(request)
+            print("GET_keywordsAnswer_from_postgres function called")
+            # Assuming you want to retrieve all records from the AnswerAndKeywords model
+            dataFromDatavase = AnswerAndKeywords.objects.all()
+            hibikiSerializer = clientGetReqSerializer(dataFromDatavase, many=True)
+            return Response(hibikiSerializer.data, status=status.HTTP_200_OK)
+        
+            
+        except ValidationError as e:
+            return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
+        except Exception as e: 
+            return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        
+    
+    def post(self, request):
+        try:
+            print(request)
+            print("POST_keywordsAnswer_from_postgres function called")
+
+            postReqSerializer = serverResSerializer(data=request.data)
+            if postReqSerializer.is_valid():
+                postReqSerializer.save()
+                # return Response(postReqSerializer.data, status=status.HTTP_201_CREATED)
+                return Response("Post data were successfully saved", status=status.HTTP_201_CREATED)
+            else:
+                return Response(postReqSerializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            
+            
+        except ValidationError as e:
+            return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
+        except Exception as e: 
+            return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        
+
+    def delete(self, request):
+        try:
+            print(request)
+            print("DELETE_keywordsAnswer_from_postgres function called")
+
+            line_to_delete = request.data.get('id')
+            if line_to_delete is None:
+                return Response({"error": "ID to be deleted is not provided"}, status=status.HTTP_400_BAD_REQUEST)
+
+            deleted = AnswerAndKeywords.objects.filter(id=line_to_delete).delete()
+            if deleted[0] == 0:
+                return Response({"error": "No record found to delete"}, status=status.HTTP_404_NOT_FOUND)
+            else:
+                return Response({"message": "Record deleted successfully"}, status=status.HTTP_200_OK)
+
+            
+            
+        except ValidationError as e:
+            return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
+        except Exception as e: 
+            return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        
+
+
+
+
+
+
+############ Original Code ############
+
+# # @permission_classes([IsAuthenticated])
+# @api_view(['GET'])
+# def get_keywordsAnswer_from_postgres(request):
+#     try:
+#         print(request)
+#         print("GET_keywordsAnswer_from_postgres function called")
+#         # Assuming you want to retrieve all records from the AnswerAndKeywords model
+#         dataFromDatavase = AnswerAndKeywords.objects.all()
+#         hibikiSerializer = clientGetReqSerializer(dataFromDatavase, many=True)
+#         return Response(hibikiSerializer.data, status=status.HTTP_200_OK)
     
 
         
-    except ValidationError as e:
-        return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
-    except Exception as e: 
-        return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+#     except ValidationError as e:
+#         return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
+#     except Exception as e: 
+#         return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
-        # return Response({"message": "Hello, world!"})
+#         # return Response({"message": "Hello, world!"})
 
 
 
-@api_view(['POST'])
-def post_keywordsAnswer_from_postgres(request):
-    try:
-        print(request)
-        print("POST_keywordsAnswer_from_postgres function called")
+# @api_view(['POST'])
+# def post_keywordsAnswer_from_postgres(request):
+#     try:
+#         print(request)
+#         print("POST_keywordsAnswer_from_postgres function called")
 
-        postReqSerializer = serverResSerializer(data=request.data)
-        if postReqSerializer.is_valid():
-            postReqSerializer.save()
-            # return Response(postReqSerializer.data, status=status.HTTP_201_CREATED)
-            return Response("Post data were successfully saved", status=status.HTTP_201_CREATED)
-        else:
-            return Response(postReqSerializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#         postReqSerializer = serverResSerializer(data=request.data)
+#         if postReqSerializer.is_valid():
+#             postReqSerializer.save()
+#             # return Response(postReqSerializer.data, status=status.HTTP_201_CREATED)
+#             return Response("Post data were successfully saved", status=status.HTTP_201_CREATED)
+#         else:
+#             return Response(postReqSerializer.errors, status=status.HTTP_400_BAD_REQUEST)
         
         
-    except ValidationError as e:
-        return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
-    except Exception as e: 
-        return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+#     except ValidationError as e:
+#         return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
+#     except Exception as e: 
+#         return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
-        # return Response({"message": "Hello, world!"})
+#         # return Response({"message": "Hello, world!"})
 
 
 
-@api_view(['DELETE'])
-def delete_keywordsAnswer_from_postgres(request):
-    try:
-        print(request)
-        print("DELETE_keywordsAnswer_from_postgres function called")
+# @api_view(['DELETE'])
+# def delete_keywordsAnswer_from_postgres(request):
+#     try:
+#         print(request)
+#         print("DELETE_keywordsAnswer_from_postgres function called")
 
-        line_to_delete = request.data.get('id')
-        if line_to_delete is None:
-            return Response({"error": "ID to be deleted is not provided"}, status=status.HTTP_400_BAD_REQUEST)
+#         line_to_delete = request.data.get('id')
+#         if line_to_delete is None:
+#             return Response({"error": "ID to be deleted is not provided"}, status=status.HTTP_400_BAD_REQUEST)
 
-        deleted = AnswerAndKeywords.objects.filter(id=line_to_delete).delete()
-        if deleted[0] == 0:
-            return Response({"error": "No record found to delete"}, status=status.HTTP_404_NOT_FOUND)
-        else:
-            return Response({"message": "Record deleted successfully"}, status=status.HTTP_200_OK)
+#         deleted = AnswerAndKeywords.objects.filter(id=line_to_delete).delete()
+#         if deleted[0] == 0:
+#             return Response({"error": "No record found to delete"}, status=status.HTTP_404_NOT_FOUND)
+#         else:
+#             return Response({"message": "Record deleted successfully"}, status=status.HTTP_200_OK)
 
         
         
-    except ValidationError as e:
-        return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
-    except Exception as e: 
-        return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)      
+#     except ValidationError as e:
+#         return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
+#     except Exception as e: 
+#         return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)     
